7a14a5df72b77331c0f8c6f027b3cdd49a8263bf
Mohd Suhail <mohdsuhail2232@gmail.com>
2023-09-10 01:16:47 +0530

    blog with db id created

---
+++ app.js
@@ -0,0 +1,152 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+const mongoose = require("mongoose");
+const multer = require("multer");
+
+const homeStartingContent =
+  "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent =
+  "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent =
+  "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/uploads"); // Set the destination folder for uploaded files
+  },
+  filename: function (req, file, cb) {
+    // Set the filename to be unique (e.g., timestamp + original name)
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+
+
+
+
+
+mongoose.connect(
+  "mongodb+srv://suhailnagore4:wBZJWpeIJ1zNVtho@cluster0.6po2xot.mongodb.net/?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+const postSchema = {
+  title: String,
+  content: String,
+  author: String,
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now, // Sets the default value to the current date and time
+  },
+};
+
+const Post = mongoose.model("Post", postSchema);
+
+
+
+
+app.get("/", function (req, res) {
+  // Fetch and render posts from MongoDB
+  Post.find({})
+    .then((posts) => {
+      res.render("home", {
+        startingContent: homeStartingContent,
+        posts: posts,
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      // Handle the error appropriately, e.g., by sending an error response
+      res.status(500).send("Internal Server Error");
+    });
+});
+
+app.get("/about", function (req, res) {
+  res.render("about", { aboutContent: aboutContent });
+});
+
+app.get("/contact", function (req, res) {
+  res.render("contact", { contactContent: contactContent });
+});
+
+app.post("/compose", upload.single("image"), function (req, res) {
+  // Check if a file was uploaded
+  if (!req.file) {
+    return res.status(400).send("No file uploaded.");
+  }
+
+  const post = new Post({
+    title: req.body.postTitle,
+    author: req.body.author,
+    content: req.body.postBody,
+    images: [req.file.filename],
+  });
+
+  post
+    .save()
+    .then(() => {
+      res.redirect("/");
+    })
+    .catch((err) => {
+      console.error(err);
+      // Handle the error appropriately, e.g., by sending an error response
+      res.status(500).send("Internal Server Error");
+    });
+});
+
+
+app.get("/compose", function (req, res) {
+  res.render("compose");
+});
+
+// ...
+
+app.get("/posts/:postId", async function (req, res) {
+  try {
+    const requestedPostId = req.params.postId;
+    const post = await Post.findOne({ _id: requestedPostId });
+    if (post) {
+      res.render("post", {
+        title: post.title,
+        author: post.author,
+        date: post.createdAt.toDateString(),
+        content: post.content,
+        images: post.images, // Pass the images array to the template
+      });
+    } else {
+      // Handle the case where the post was not found, e.g., by rendering an error page
+      res.status(404).render("error", { message: "Post not found" });
+    }
+  } catch (err) {
+    console.error(err);
+    // Handle any other errors appropriately, e.g., by rendering an error page
+    res.status(500).render("error", { message: "Internal Server Error" });
+  }
+});
+
+
+// ...
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
